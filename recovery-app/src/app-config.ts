e77d8d673bb9857b9977e35b9bf41277266ba533
chen <23054115+cwkang1998@users.noreply.github.com>
2025-04-05 16:49:55 +0800

    chore: update endpoint for frontend

--- recovery-app/src/app-config.ts
+++ recovery-app/src/app-config.ts
@@ -1,5 +1,5 @@
 export const appConfig = {
   appName: '7702 Recoverable',
   scope: '7702-recoverable-17135',
-  endpoint: 'https://b235-111-235-226-130.ngrok-free.app/verify',
+  endpoint: 'https://aa9e-111-235-226-130.ngrok-free.app/verify',
 };
