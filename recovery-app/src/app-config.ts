cd57fd9e2e4b0391d590f7e10c996de4dcdc1b4a
chen <23054115+cwkang1998@users.noreply.github.com>
2025-04-05 13:36:03 +0800

    chore: setup ngrok public for use

--- recovery-app/src/app-config.ts
+++ recovery-app/src/app-config.ts
@@ -1,5 +1,5 @@
 export const appConfig = {
   appName: '7702 Recoverable',
   scope: '7702-recoverable-17135',
-  endpoint: 'https://da7a-111-235-226-130.ngrok-free.app/verify',
+  endpoint: 'https://95a7-111-235-226-130.ngrok-free.app/verify',
 };
