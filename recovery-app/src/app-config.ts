85e4cbca04abf31dc6e4da4389783d7a801958b4
chen <23054115+cwkang1998@users.noreply.github.com>
2025-04-05 16:30:34 +0800

    chore: update server url

--- recovery-app/src/app-config.ts
+++ recovery-app/src/app-config.ts
@@ -1,5 +1,5 @@
 export const appConfig = {
   appName: '7702 Recoverable',
   scope: '7702-recoverable-17135',
-  endpoint: 'https://650d-111-235-226-130.ngrok-free.app/verify',
+  endpoint: 'https://b235-111-235-226-130.ngrok-free.app/verify',
 };
