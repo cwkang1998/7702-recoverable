62200289858e273ca15efaf159354c1cd0507c31
eerkaijun <kaijuneer@gmail.com>
2025-04-05 14:15:28 +0800

    deploy contracts

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # 7702-middleware
+
+### Deploying contracts
+
+```
+cd contracts
+forge script scripts/Deploy.s.sol:Deploy --rpc-url sepolia --broadcast
+```
+
+ExperimentDelegation deployed to: `0xE117313ee57a33567A03DcEA7bf23A3669e5C3f3`
+ExperimentERC20 deployed to: `0xcf89cdAfE88d13a2B96710c9709B7E6D7Aa654E5`
---
+++ contracts/scripts/Deploy.s.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.23;
+
+import {Script} from "forge-std/Script.sol";
+import {console} from "forge-std/console.sol";
+import {ExperimentDelegation} from "../src/ExperimentDelegation.sol";
+import {ExperimentERC20} from "../src/ExperimentERC20.sol";
+
+contract Deploy is Script {
+    function run() public {
+        // Get deployment private key from environment
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+
+        // Start broadcasting transactions
+        vm.startBroadcast(deployerPrivateKey);
+
+        // Deploy ExperimentDelegation
+        ExperimentDelegation delegation = new ExperimentDelegation();
+
+        // Deploy ExperimentERC20 with contract deployer as authorized origin
+        ExperimentERC20 token = new ExperimentERC20(address(msg.sender));
+
+        vm.stopBroadcast();
+
+        // Log deployed addresses
+        console.log("ExperimentDelegation deployed to:", address(delegation));
+        console.log("ExperimentERC20 deployed to:", address(token));
+    }
+}
